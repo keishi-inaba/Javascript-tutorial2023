7ab7d674a9aedfa574f7a6a87a3f0d8abd9520a7
keishi.i <gajyumaru1103@gmail.com>
2023-02-16 17:07:49 +0900

    JavaScript-tutorial-site

---
+++ app.js
@@ -0,0 +1,20 @@
+const circle = document.getElementById('circle');
+const upBtn = document.getElementById('upBtn');
+const downBtn = document.getElementById('downBtn');
+
+let rotateValue = circle.style.transform;
+let rotateSum;
+
+upBtn.onclick = function() {
+  // 時計回りに90度回転
+  rotateSum = rotateValue + "rotate(-90deg)";
+  circle.style.transform = rotateSum;
+  rotateValue = rotateSum;
+}
+
+downBtn.onclick = function() {
+  // 半時計回りに90度回転
+  rotateSum = rotateValue + "rotate(90deg)";
+  circle.style.transform = rotateSum;
+  rotateValue = rotateSum;
+}
